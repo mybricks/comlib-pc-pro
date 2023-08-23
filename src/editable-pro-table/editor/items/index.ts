7c207146f68bac22d835ed1e04034027d2836466
zhuba-Ahhh <3477826311@qq.com>
2023-08-23 10:21:07 +0800

    feat(editable-table): seclect dataSource config

--- src/editable-pro-table/editor/items/index.ts
+++ src/editable-pro-table/editor/items/index.ts
@@ -1,4 +1,5 @@
-import { Data } from '../../constants';
+import { Data, TypeEnum } from '../../constants';
+import { checkType, getCol, setCol } from '../../utils';
 import indexEditor from '../indexEditor';
 import baseEditor from './baseEditor';
 import styleEditor from './styleEditor';
@@ -8,6 +9,7 @@ import operationRowEditor from './operationRowEditor';
 import dateEditor from './dateEditor';
 import selectEditor from './selectEditor';
 import proEditor from './proEditor';
+import { uuid } from '../../../utils';
 
 export const COLUMN_EDITORS_CLASS_KEY =
   '.ant-table-thead .ant-table-cell:not(.ant-table-selection-column):not(.ant-table-cell-scrollbar):not(.ant-table-row-expand-icon-cell):not(.column-draggle)';
@@ -16,6 +18,50 @@ export default {
     cate1.title = '常规';
     cate1.items = [
       baseEditor,
+      {
+        title: '枚举数据源',
+        type: 'array',
+        options: {
+          addText: '添加枚举值',
+          editable: true,
+          draggable: true,
+          getTitle(item) {
+            return `${item.label} - ${item.value}`;
+          },
+          onAdd() {
+            const value = uuid();
+            return {
+              label: '选项',
+              value
+            };
+          },
+          items: [
+            {
+              title: '选项标签',
+              type: 'text',
+              value: 'label'
+            },
+            {
+              title: '选项值',
+              type: 'valueSelect',
+              options: ['text', 'number', 'boolean'],
+              description: '选项的唯一标识，可以修改为有意义的值',
+              value: 'value'
+            }
+          ]
+        },
+        ifVisible({ data, focusArea }: EditorResult<Data>) {
+          return checkType(data, focusArea, [TypeEnum.Select, TypeEnum.Checkbox]);
+        },
+        value: {
+          get({ data, focusArea }: EditorResult<Data>) {
+            return getCol(data, focusArea, 'valueEnum');
+          },
+          set({ data, focusArea }: EditorResult<Data>, val: Array<any>) {
+            setCol(data, focusArea, 'valueEnum', val);
+          }
+        }
+      },
       indexEditor
     ];
 
